3625844660e7fe6c4df1e32bc7f6cf0063f2df63
Anja Reusch <anja.reusch@tu-dresden.de>
2023-10-18 17:52:58 +0200

    removing more files

--- jlama-core/src/main/java/com/github/tjake/jlama/safetensors/WeightLoader.java
+++
@@ -1,9 +0,0 @@
-package com.github.tjake.jlama.safetensors;
-
-import com.github.tjake.jlama.tensor.AbstractTensor;
-
-public interface WeightLoader {
-    AbstractTensor load(String name);
-
-    DType getModelDType();
-}
