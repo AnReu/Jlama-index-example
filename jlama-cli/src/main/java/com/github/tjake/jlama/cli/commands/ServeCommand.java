19169f24ef0cac3705761a52e2fe85c9c1a00d59
Anja Reusch <anja.reusch@tu-dresden.de>
2023-10-18 16:54:48 +0200

    remove parts not needed for demo

--- jlama-cli/src/main/java/com/github/tjake/jlama/cli/commands/ServeCommand.java
+++
@@ -1,7 +0,0 @@
-package com.github.tjake.jlama.cli.commands;
-
-import picocli.CommandLine;
-
-@CommandLine.Command(name = "serve", description = "Starts a rest api for interacting with this model")
-public class ServeCommand extends BaseCommand {
-}
--- models/README.md
+++
@@ -1,8 +0,0 @@
-### HuggingFace Models for Jlama
-
-Use the `download_hf_models.sh` script to download the models.
-
-```shell
-./download_hf_models.sh gpt2-medium
-./download_hf_models.sh -a XXXXXXXX meta-llama/Llama-2-7b-chat-hf
-```
